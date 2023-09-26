77bb12fedf7c7f43f19ea841950236ffe86ee53b
Primate <bastien.fremondiere@agence-ohana.fr>
2023-09-27 00:48:13 +0200

    Disable SSR for Gallery

--- src/app/gallery.tsx
+++ src/app/gallery.tsx
@@ -2,36 +2,35 @@
 import Image from 'next/image'
 import ALTERED from '../altered'
 import { useState, useEffect } from 'react';
+import { useLocalStorage } from 'usehooks-ts'
 
 type AlteredEntity =
     {
         name: string;
         entityId: number,
         order: number,
-        rarity: number
+        rarity: number,
+        hideSelected: boolean
     };
 
-function AlteredIcon({ name, entityId, order, rarity }: AlteredEntity) {
-    const [selected, setSelected] = useState(localStorage.getItem("selected-" + entityId) === "true" ? true : false);
-    const [chroma, setChroma] = useState(localStorage.getItem("chroma-" + entityId) === "true" ? true : false);
-    useEffect(() => {
-        localStorage.setItem("selected-" + entityId, selected ? "true" : "false");
-    }, [selected]);
+function AlteredIcon({ name, entityId, order, rarity, hideSelected }: AlteredEntity) {
+    const [selected, setSelected] = useLocalStorage("selected-" + entityId, false);
+    const [chroma, setChroma] = useLocalStorage("chroma-" + entityId, false);
 
-    useEffect(() => {
-        localStorage.setItem("chroma-" + entityId, selected ? "true" : "false");
-    }, [chroma]);
     return (
         <div style={{
             width: 150,
             height: 150
         }}
-            className={"relative block hover:cursor-pointer rounded-lg bg-zinc-900 hover:bg-zinc-800 box-border duration-100 " + (selected ? "border-solid border-2 border-sky-500" : "")} >
+
+            className={"relative block hover:cursor-pointer rounded-lg bg-zinc-900 hover:bg-zinc-800 box-border duration-100 " + (selected ? "border-solid border-2 border-sky-500 " : "") + ((selected && hideSelected) ? " hidden" : "")
+            } >
             <div className=' w-8 h-8 text-center text-lg bg-zinc-700 rounded-lg absolute z-20' onClick={() => setChroma(!chroma)}>{chroma ? "★" : "☆"}</div>
             <Image alt={name}
                 className='absolute z-0 left-0 top-0'
                 onClick={() => setSelected(!selected)}
                 src={"/altered/" + entityId + "." + "Chroma" + ".png"}
+
                 width={150}
                 height={150} />
             <Image alt={name}
@@ -39,19 +38,28 @@ function AlteredIcon({ name, entityId, order, rarity }: AlteredEntity) {
                 onClick={() => setSelected(!selected)}
                 src={"/altered/" + entityId + "." + "Normal" + ".png"}
                 width={150}
-                height={150} />
+                height={150}
+            />
+            <div className="text-sm absolute right-0 bottom-0 z-20 bg-zinc-700 opacity-80 p-1 rounded-lg">{name}</div>
         </div >
     )
 }
 
-export default function Gallery() {
+export default function Gallery({ hideSelected }: any) {
     return (
         <div className="flex w-10/12 flex-wrap align-middle gap-y-6 gap-x-6">
-            {ALTERED.map(altered => {
-                return (
-                    <AlteredIcon {...altered} />
-                )
-            })}
+            {ALTERED.filter((altered) => {
+                if (hideSelected) {
+                    return localStorage.getItem("selected-" + altered.entityId) === "false"
+                } else {
+                    return true;
+                }
+            })
+                .map(altered => {
+                    return (
+                        <AlteredIcon {...altered} hideSelected={hideSelected} />
+                    )
+                })}
         </div>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-import Image from 'next/image'
-import Gallery from './gallery'
+"use client";
+import { useState } from 'react';
+import dynamic from 'next/dynamic'
+const Gallery = dynamic(() => import('./gallery'), { ssr: false })
 
 export default function Home() {
+  const [hideSelected, setHideSelected] = useState(false);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <h1 className=' uppercase font-bold tracking-wide p-16 text-2xl'>MobDex Osatopia 2</h1>
-      <Gallery />
+      <div className="flex items-center mb-4">
+        <input id="default-checkbox" type="checkbox" checked={hideSelected} onChange={(event) => setHideSelected(event.target.checked)} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+        <label htmlFor="default-checkbox" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Hide selected</label>
+      </div>
+      <Gallery hideSelected={hideSelected} />
     </main>
   )
 }
