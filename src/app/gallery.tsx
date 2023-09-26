bec18a2d3182452b4bfabebed904ce1efa1578f0
Primate <bastien.fremondiere@agence-ohana.fr>
2023-09-27 00:18:02 +0200

    Make chroma and selection work

--- src/app/gallery.tsx
+++ src/app/gallery.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from 'next/image'
 import ALTERED from '../altered'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 type AlteredEntity =
     {
@@ -12,13 +12,35 @@ type AlteredEntity =
     };
 
 function AlteredIcon({ name, entityId, order, rarity }: AlteredEntity) {
-    const [selected, setSelected] = useState(false);
+    const [selected, setSelected] = useState(localStorage.getItem("selected-" + entityId) === "true" ? true : false);
+    const [chroma, setChroma] = useState(localStorage.getItem("chroma-" + entityId) === "true" ? true : false);
+    useEffect(() => {
+        localStorage.setItem("selected-" + entityId, selected ? "true" : "false");
+    }, [selected]);
+
+    useEffect(() => {
+        localStorage.setItem("chroma-" + entityId, selected ? "true" : "false");
+    }, [chroma]);
     return (
-        <Image className="flex " alt={name}
-            onClick={() => setSelected(!selected)}
-            src={"/altered/" + entityId + "." + (selected ? "Chroma" : "Normal") + ".png"}
-            width={150}
-            height={150} />
+        <div style={{
+            width: 150,
+            height: 150
+        }}
+            className={"relative block hover:cursor-pointer rounded-lg bg-zinc-900 hover:bg-zinc-800 box-border duration-100 " + (selected ? "border-solid border-2 border-sky-500" : "")} >
+            <div className=' w-8 h-8 text-center text-lg bg-zinc-700 rounded-lg absolute z-20' onClick={() => setChroma(!chroma)}>{chroma ? "★" : "☆"}</div>
+            <Image alt={name}
+                className='absolute z-0 left-0 top-0'
+                onClick={() => setSelected(!selected)}
+                src={"/altered/" + entityId + "." + "Chroma" + ".png"}
+                width={150}
+                height={150} />
+            <Image alt={name}
+                className={'left-0 top-0 absolute z-10 duration-300 ' + (chroma ? "opacity-0" : "opacity-100")}
+                onClick={() => setSelected(!selected)}
+                src={"/altered/" + entityId + "." + "Normal" + ".png"}
+                width={150}
+                height={150} />
+        </div >
     )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Gallery from './gallery'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <h1 className=' uppercase font-bold tracking-wide p-16 text-2xl'>MobDex Osatopia 2</h1>
       <Gallery />
     </main>
   )
